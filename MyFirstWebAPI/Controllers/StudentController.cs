aff6aac19ede9372e3162a10041f60e00c8f7b87
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-12-18 17:52:57 +0530

    My first web api project

---
+++ MyFirstWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPITraining.Models;
+
+namespace WebAPITraining.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("All")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Student>> GetStudentList()
+        {
+            return Ok(College.Students);
+        }
+
+        [HttpGet("id:int", Name = "GetStudent")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Student> GetStudentList(int id)
+        {
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+            var studnet = College.Students.FirstOrDefault(x => x.Id == id);
+            if (studnet == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(studnet);
+        }
+
+        [HttpGet("name:alpha", Name = "GetStudentByName")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Student> GetStudentByName(string name)
+        {
+            if (name == null)
+            {
+                return BadRequest();
+            }
+            var student = College.Students.FirstOrDefault(x => x.Name == name);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<Student> CreateStudent([FromBody] Student student)
+        {
+            if (student == null)
+            {
+                return BadRequest(student);
+            }
+            if (student.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            student.Id = College.Students.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
+            College.Students.Add(student);
+
+            return CreatedAtRoute("GetStudent", new { id = student.Id }, student);
+        }
+
+
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Student> DeleteStudent(int id)
+        {
+            var student = College.Students.FirstOrDefault(x => x.Id == id);
+            if(student == null)
+            {
+                return NotFound(student);
+            }
+            College.Students.Remove(student);
+            return Ok(student);
+        }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult UpdateStudent(int id, [FromBody] Student updatedStudent)
+        {
+            var existingStudent = College.Students.FirstOrDefault(s => s.Id == id);
+
+            if (existingStudent == null)
+            {
+                return NotFound(); 
+            }
+
+            // Update the existing student's properties
+            existingStudent.Name = updatedStudent.Name;
+            existingStudent.Age = updatedStudent.Age;
+            existingStudent.Department = updatedStudent.Department;
+
+            return Ok(existingStudent);
+        }
+    }
+}
---
+++ MyFirstWebAPI/Models/College.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPITraining.Models
+{
+    public class College
+    {
+        public static IList<Student> Students = new List<Student>()
+        {
+            new Student() { Id = 1, Name = "Akshat", Age = 21, Department = "IT" },
+            new Student() { Id = 2, Name = "Bohra", Age = 23, Department = "CSE" },
+            new Student() { Id = 3, Name = "Govind", Age = 22, Department = "SE" },
+            new Student() { Id = 4, Name = "Yash", Age = 20, Department = "ECE" },
+        };
+    }
+}
