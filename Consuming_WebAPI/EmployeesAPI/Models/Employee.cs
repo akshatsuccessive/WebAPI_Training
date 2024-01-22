aeca403534991187fa9ecd9fd6ddf946fd9d9acb
akshat.kumar <akshat.kumar@successive.tech>
2024-01-22 18:34:23 +0530

    Employee web api

---
+++ Consuming_WebAPI/EmployeesAPI/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesAPI.Models
+{
+    public class Employee
+    {
+        [ValidateNever]
+        public int Id { get; internal set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
