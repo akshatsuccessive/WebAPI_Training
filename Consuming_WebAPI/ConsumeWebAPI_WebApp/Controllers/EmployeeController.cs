9257a4158487f07efee3856ef4293268947462fd
akshat.kumar <akshat.kumar@successive.tech>
2024-01-22 18:37:38 +0530

    Consuming Web API on ASP.NET core MVC project

---
+++ Consuming_WebAPI/ConsumeWebAPI_WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using ConsumeWebAPI_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumeWebAPI_WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7150/api");
+        
+        // HttpClient
+        private readonly HttpClient _httpClient;
+        public EmployeeController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Employee> employeesList = new List<Employee>();
+            HttpResponseMessage respone = _httpClient.GetAsync(_httpClient.BaseAddress + "/Employee/GetAllEmployees").Result;  // controllerName/Action method
+
+            if(respone.IsSuccessStatusCode)
+            {
+                string data = respone.Content.ReadAsStringAsync().Result;
+
+                // deserialize the string data to json
+                employeesList = JsonConvert.DeserializeObject<List<Employee>>(data);
+            }
+            return View(employeesList);
+        }
+    }
+}
---
+++ Consuming_WebAPI/ConsumeWebAPI_WebApp/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsumeWebAPI_WebApp.Models
+{
+    public class Employee
+    {
+        [ValidateNever]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Employee Name")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
